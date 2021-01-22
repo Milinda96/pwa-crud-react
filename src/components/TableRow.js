5ac8f70c546779ffba6db993d5aa9c0bdd9f6b75
Milinda Senaka <milindasenaka.1@gmail.com>
2021-01-22 20:28:18 +0530

    edit student data

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import { Link } from 'react-router-dom';
 class TableRow extends Component {
     render() {
         return (
@@ -7,7 +7,7 @@ class TableRow extends Component {
                     <td>{this.props.obj.student_name}</td>
                     <td>{this.props.obj.school_name}</td>
                     <td>{this.props.obj.class_name}</td>
-                    <td><button className="btn btn-primary">Edit</button></td>
+                    <td><Link to={"/edit/" + this.props.obj._id} className="btn btn-primary">Edit</Link></td>
                     <td><button className="btn btn-primary">Delete</button></td>
                 </tr>
         );
--- src/components/edit.component.js
+++ src/components/edit.component.js
@@ -1,12 +1,72 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 export default class Edit extends Component {
-    render()  {
+    constructor(props) {
+        super(props);
+        this.onChangeStudentName = this.onChangeStudentName.bind(this);
+        this.onChangeSchooName = this.onChangeSchooName.bind(this);
+        this.onChangeClassName = this.onChangeClassName.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            student_name: '',
+            school_name: '',
+            class_name: ''
+        };
+    }
+    componentDidMount() {
+        axios.get('http://localhost:4000/student/edit/' + this.props.match.params._id).then(
+            res => {
+                this.setState({
+                    student_name: res.data.student_name,
+                    school_name: res.data.school_name,
+                    class_name: res.data.class_name
+                });
+            }
+        ).catch((err) => {
+            console.log(err);
+        });
+    }
+
+    onChangeStudentName(e) {
+        this.setState({
+            student_name: e.target.value,
+        });
+    }
+    onChangeSchooName(e) {
+        this.setState({
+            school_name: e.target.value,
+        });
+    }
+    onChangeClassName(e) {
+        this.setState({
+            class_name: e.target.value,
+        });
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        const obj = {
+            student_name: this.state.student_name,
+            school_name: this.state.school_name,
+            class_name: this.state.class_name
+        };
+        axios.post('http://localhost:4000/student/edit/' + this.props.match.params._id, obj).then(
+            res => {
+                console.log(res.data);
+            });
+        this.props.history.push('/index');
+    }
+    render() {
         return (
-            <div>
-                <p>
-                    Welcome to Edit
-                </p>
+            <div
+                style={{
+                    marginTop: 50,
+                    marginLeft: 100,
+                    marginRight: 100,
+                }}
+            >
+                <h3> Edit Student </h3>
             </div>
         );
     }
